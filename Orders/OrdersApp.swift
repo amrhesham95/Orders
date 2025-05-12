aa4071b880f23e625248610bdfe64c25f434e29f
AmrHassan95 <amr.hassan.2@deliveryhero.com>
2025-05-12 20:00:06 +0300

    Initial Commit

---
+++ Orders/OrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrdersApp.swift
+//  Orders
+//
+//  Created by Amr Hassan on 12.05.25.
+//
+
+import SwiftUI
+
+@main
+struct OrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrdersTests/OrdersTests.swift
@@ -0,0 +1,16 @@
+//
+//  OrdersTests.swift
+//  OrdersTests
+//
+//  Created by Amr Hassan on 12.05.25.
+//
+
+import Testing
+
+struct OrdersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
